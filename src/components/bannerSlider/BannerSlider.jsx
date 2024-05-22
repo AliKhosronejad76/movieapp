2ee2cd91ca5e0a2dc10b7ed51bd8e8585841dac4
Ali Khosronejad <khosronejadali344@gmail.com>
2024-05-22 13:26:26 +0330

    banner slider created with static data

--- src/components/bannerSlider/BannerSlider.jsx
+++ src/components/bannerSlider/BannerSlider.jsx
@@ -2,10 +2,32 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import Item from "./Item";
+import { IoIosArrowBack } from "react-icons/io"
+import { IoIosArrowForward } from "react-icons/io"
 export default function BannerSlider(){
+
+  
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div onClick={onClick} className="cursor-pointer bg-[#000] opacity-90 rounded-full w-[44px] h-[44px] lg:w-[55px] lg:h-[55px] flex items-center justify-center absolute text-white top-[41%] left-[7px] text-xl lg:text-4xl ">
+        <IoIosArrowBack/>
+      </div>
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { onClick } = props;
+    return (
+      <div onClick={onClick} className="cursor-pointer bg-[#000] opacity-90 rounded-full w-[44px] h-[44px] lg:w-[55px] lg:h-[55px] flex items-center justify-center absolute text-white top-[41%] right-[7px] text-xl lg:text-4xl z-30">
+        <IoIosArrowForward/>
+      </div>
+    );
+  }
+
     var settings = {
-        dots: true,
+        dots: false,
         fade: true,
         infinite: true,
         speed: 500,
@@ -13,12 +35,16 @@ export default function BannerSlider(){
         slidesToScroll: 1,
         waitForAnimate: false,
         autoplay: true,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
        
       };
 
     return(
         <Slider {...settings} >
-          
+          <Item img={"/img/bg.jpeg"}/>
+          <Item img={"https://cdn.nody.ir/files/2021/06/16/nody-%D8%B9%DA%A9%D8%B3-%D9%84%D8%A7%DA%A9-%D9%BE%D8%B4%D8%AA-%D9%87%D8%A7%DB%8C-%D9%86%DB%8C%D9%86%D8%AC%D8%A7-%D8%B1%D8%A7%D9%81%D8%A7%D8%A6%D9%84-1623854849.jpg"}/>
+          <Item img={"https://cdn.nody.ir/files/2021/06/16/nody-%D8%B9%DA%A9%D8%B3-%D8%B5%D9%81%D8%AD%D9%87-%D9%84%D8%A7%DA%A9-%D9%BE%D8%B4%D8%AA-%D9%87%D8%A7%DB%8C-%D9%86%DB%8C%D9%86%D8%AC%D8%A7-1623854841.jpg"}/>
          
         </Slider>
     )
---
+++ src/components/bannerSlider/Item.jsx
@@ -0,0 +1,25 @@
+export default function Item({img}){
+    return(
+        <div className="relative h-[500px]">
+            <div className="bg-[#000] opacity-50 absolute top-0 bottom-0 right-0 left-0 z-40 w-full h-full"></div>
+            <img src={img} className="relative h-full w-full object-cover bg-center " />
+
+            <div className="absolute z-50 top-20 bottom-0 left-0 right-0  flex items-start justify-start px-2 md:px-4 lg:px-12">
+                
+                <div className="w-full text-white ">
+                    <h1 className="text-3xl  lg:text-5xl mb-4">Lord of Rings III</h1>
+
+                    <p className="mt-5 text-justify md:px-2 leading-loose text-sm lg:text-lg w-full md:w-[95%] lg:w-[70%]">
+                    The Lord of the Rings is an epic[1] high fantasy novel by the English author and scholar J. R. R. Tolkien. Set in Middle-earth, the story began as a sequel to Tolkien's 1937 children's book The Hobbit, but eventually developed into a much larger work. Written in stages between 1937 and 1949, The Lord of the Rings is one of the best-selling books ever written, with over 150 million 
+                    </p>
+
+                    <div className="mt-4 px-2.5 py-2 ">
+                        <button className="bg-red-600 text-white text-xl rounded-sm w-[100px] h-[40px] lg:w-[160px] lg:h-[47px] transition-all duration-400 text-base lg:text-lg hover:bg-red-800 ">Watch</button>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
